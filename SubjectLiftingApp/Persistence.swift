9f1869de1fd754ada03300b5ca0300f954fccc4f
Christoffer Lund <chrislund90@gmail.com>
2023-06-18 04:16:18 +0200

    Added cloudkit support

--- SubjectLiftingApp/Persistence.swift
+++ SubjectLiftingApp/Persistence.swift
@@ -33,14 +33,16 @@ struct PersistenceController {
         return result
     }()
 
-    let container: NSPersistentContainer
+    let container: NSPersistentCloudKitContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "SubjectLiftingApp")
+        container = NSPersistentCloudKitContainer(name: "SubjectLiftingApp")
+
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+
+        container.loadPersistentStores(completionHandler: { (_, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -56,6 +58,7 @@ struct PersistenceController {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
